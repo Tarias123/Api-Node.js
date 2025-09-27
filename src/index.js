fbfa4de771d31b3e335b4e1b1190bbab8d592f0f
Angel Tapia <angeltapia1808@gmail.com>
2025-09-26 22:37:52 -0600

    paguinas ya enlazadas falta colocarles los datos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import express from 'express';
 import cines from './routers/cines.routes.js';
 import peliculas from './routers/peliculas.routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','./src/views');
+app.use(express.static('src/public'));                                                        
+app.get('/', (req, res) => {
+  res.render('index', { titulo: 'Inicio' });
+});
+
+app.get('/cines', (req, res) => {
+  res.render('cines', { titulo: 'Nuestros Cines' });
+});
+
+app.get('/peliculas', (req, res) => {
+  res.render('peliculas', { titulo: 'Cartelera' });
+});
+
+app.get('/peliculas/:id', (req, res) => {
+  res.render('pelicula', { titulo: 'Detalle de la Película' });
+});
+
+app.get('/cines', (req, res) => {
+  res.render('cine', { titulo: 'Cine' });
+});
+
+app.get('/cines/:id', (req, res) => {
+  res.render('cine', { titulo: 'Detalle del Cine' });
+});
+
+
 
 
 app.use(cines);
 app.use(peliculas);
+
 
 app.listen(3000, () => {
   console.log('Servidor escuchando en http://localhost:3000');
